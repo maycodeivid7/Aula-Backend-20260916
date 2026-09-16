2c3242f9d490f7acf6f495a2e684d4da61669a62
Mayco Deivid <mayco.deivid@webjoi.com.br>
2026-09-16 19:59:41 -0300

    Commit Inicial

---
+++ index.js
@@ -0,0 +1,11 @@
+// npm i express
+// npm init
+
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
